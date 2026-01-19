b2bc06605faf40a630feb352f48c5cd394d3b460
John Eric T. Orolfo <jorolfo@transurban.com>
2026-01-19 17:46:47 +0800

    Configure dynamic base URL based on build mode.

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [
-    solid(),
-    tailwindcss()
-  ],
-  assetsInclude: ['**/*.svg'],
-  base: "/gogma-tracker/",
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
+export default defineConfig(async ({mode}) => {
+  return {
+    plugins: [
+      solid(),
+      tailwindcss()
+    ],
+    assetsInclude: ['**/*.svg'],
+    base: mode === "prod" ? '/gogma-tracker/' : './',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
       },
     },
-  },
+  }
 })
