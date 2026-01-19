0a5fdcd2cadad081de6289f618ca3998e8829d61
John Eric T. Orolfo <jorolfo@transurban.com>
2026-01-19 17:39:24 +0800

    Fix SVG paths to use BASE_URL for production deployment.

--- src/utils/WeaponImageUtil.ts
+++ src/utils/WeaponImageUtil.ts
@@ -1,17 +1,19 @@
-const bow = '/weapons/Bow/Bow_Rank_01.svg'
-const chargeBlade = '/weapons/Charge_Blade/Charge_Blade_rank_01.svg'
-const dualBlades = '/weapons/Dual_Blades/Dual_Blades_Rank_01.svg'
-const greatSword = '/weapons/Great_Sword/Great_Sword_Rank_01.svg'
-const gunLance = '/weapons/Gunlance/Gunlance_Rank_01.svg'
-const hammer = '/weapons/Hammer/Hammer_Rank_01.svg'
-const heavyBowGun = '/weapons/Heavy_Bowgun/Heavy_Bowgun_Rank_01.svg'
-const huntingHorn = '/weapons/Hunting_Horn/Hunting_Horn_Rank_01.svg'
-const insectGlaive = '/weapons/Insect_Glaive/Insect_Glaive_Rank_01.svg'
-const lance = '/weapons/Lance/Lance_Rank_01.svg'
-const lightBowGun = '/weapons/Light_Bowgun/Light_Bowgun_Rank_01.svg'
-const longSword = '/weapons/Long_Sword/Long_Sword_Rank_01.svg'
-const swordAndShield = '/weapons/Sword_and_Shield/Sword_&_Shield_Rank_01.svg'
-const switchAxe = '/weapons/Switch_Axe/Switch_Axe_Rank_01.svg'
+const base = import.meta.env.BASE_URL
+
+const bow = `${base}weapons/Bow/Bow_Rank_01.svg`
+const chargeBlade = `${base}weapons/Charge_Blade/Charge_Blade_rank_01.svg`
+const dualBlades = `${base}weapons/Dual_Blades/Dual_Blades_Rank_01.svg`
+const greatSword = `${base}weapons/Great_Sword/Great_Sword_Rank_01.svg`
+const gunLance = `${base}weapons/Gunlance/Gunlance_Rank_01.svg`
+const hammer = `${base}weapons/Hammer/Hammer_Rank_01.svg`
+const heavyBowGun = `${base}weapons/Heavy_Bowgun/Heavy_Bowgun_Rank_01.svg`
+const huntingHorn = `${base}weapons/Hunting_Horn/Hunting_Horn_Rank_01.svg`
+const insectGlaive = `${base}weapons/Insect_Glaive/Insect_Glaive_Rank_01.svg`
+const lance = `${base}weapons/Lance/Lance_Rank_01.svg`
+const lightBowGun = `${base}weapons/Light_Bowgun/Light_Bowgun_Rank_01.svg`
+const longSword = `${base}weapons/Long_Sword/Long_Sword_Rank_01.svg`
+const swordAndShield = `${base}weapons/Sword_and_Shield/Sword_&_Shield_Rank_01.svg`
+const switchAxe = `${base}weapons/Switch_Axe/Switch_Axe_Rank_01.svg`
 
 export const getWeaponIcons = (weaponName: string) => {
   switch (weaponName) {
